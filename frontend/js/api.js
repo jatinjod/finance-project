36f3f160fc6a1a91fbb35d956be4e1f6f3c0e99e
Jatin <jatinnkumar007@gmail.com>
2026-09-12 15:38:54 +0530

    Fix backend URL

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -142,7 +142,7 @@ async function apiRequest(endpoint, method = 'GET', data = null) {
         return {
             error:
                 'Cannot connect to server. ' +
-                'Make sure the backend is running on https://finance-project-t8zo.onrender.com',
+                'Make sure the backend is running on https://finance-project-zf24.onrender.com',
             status_code: 0
         };
     }
